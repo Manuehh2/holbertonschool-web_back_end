854baf2009e221a4d449edaa266a4d14a3324c1c
Manuel Fajardo <39359956+Manuehh2@users.noreply.github.com>
2026-02-20 18:18:35 -0600

    Delete ES6_promise directory

--- ES6_promise/1-promise.js
+++
@@ -1,6 +0,0 @@
-export default function getFullResponseFromAPI(success) {
-  if (success === true) {
-    return Promise.resolve({ status: 200, body: 'Success' });
-  }
-  return Promise.reject(new Error('The fake API is not working currently'));
-}
