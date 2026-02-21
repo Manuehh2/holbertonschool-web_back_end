2cc9343f9eafe351c011fd61124df2f86fe3ea89
Manuel Fajardo <39359956+Manuehh2@users.noreply.github.com>
2026-02-20 19:41:08 -0600

    Refactor Glam rock lifespan query
    Updated query to use IFNULL and REGEXP for better handling of lifespan calculation.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,2 @@
--- Lists all bands with Glam rock as their main style,
--- ranked by their longevity up to 2024
-
-SELECT
-    band_name,
-    2024 - formed AS lifespan
-FROM metal_bands
-WHERE style LIKE 'Glam rock%'
-ORDER BY lifespan DESC;
+-- Hola
+SELECT band_name, (IFNULL(split, 2024) -  formed) AS lifespan FROM metal_bands WHERE style REGEXP 'Glam rock' ORDER BY lifespan DESC;
